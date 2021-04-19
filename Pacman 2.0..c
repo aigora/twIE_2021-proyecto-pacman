3fbbdc3ef79b942c61ba27ff4ee364a9d3d2d6c2
Alvaro Lopez-Hermida <alvaro.lopez-hermida@alumnos.upm.es>
2021-04-19 09:53:29 +0200

    Menú sin windows.h y conio.h
    Eliminacion de las librerias de windows.h y conio.h para que se pueda ejecutar en cualquier ordenador

--- Pacman 2.0..c
+++ Pacman 2.0..c
@@ -1,88 +1,29 @@
-// Aqui empezara el código
-#include<windows.h>
 #include <stdio.h>
-#include<conio.h>
+
+//Aqui definimos las funciones de los tres modos de juego
 
 int mododejuego1(int a, int b);
 int mododejuego2(int c, int d);
 int mododejuego3(int e, int f);
-//Aqui definimos las funciones de los tres modos de juego
 
-void main()
+
+int main()
 {
     int seleccion;
-    system("COLOR B0");
-    printf("\033[0;31m");
+
+    //Aqui el menu nos da a elegir entre los 3 modos de juego
+
+    printf("***************************************************************\n\n");
+    printf("                   Eliga el modo de juego                    \n\n");
+    printf("  -Para modo de juego 1 pulse 1 e intro\n");
+    printf("  -Para modo de juego 2 pulse 2 e intro\n");
+    printf("  -Para modo de juego 3 pulse 3 e intro\n");
+    printf("  -Para cerrar programa pulse cualquier otra tecla e intro\n\n");
     printf("***************************************************************\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[4;32m");
-    printf("                   Eliga el modo de juego                    ");
-    printf("\033[0;35m");
-    printf("*\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[1;35m");
-    printf("  -Para modo de juego 1 pulse 1 e intro");
-    printf("\033[0;35m");
-    printf("                      *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[1;33m");
-    printf("  -Para modo de juego 2 pulse 2 e intro");
-    printf("\033[0;35m");
-    printf("                      *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[1;36 m");
-    printf("  -Para modo de juego 3 pulse 3 e intro");
-    printf("\033[0;35m");
-    printf("                      *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;37m");
-    printf("  -Para cerrar probrama pulse cualquier otra tecla e intro");
-    printf("\033[0;35m");
-    printf("   *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;36m");
-    printf("*");
-    printf("\033[0;35m");
-    printf("                                                             *\n");
-    printf("\033[0;32m");
-    printf("***************************************************************\n");
-    printf("\033[0m");
 
     scanf("%i",&seleccion);
 
-    //Aqui el menu nos da a elegir entre los 3 modos de juego con un bucle
+    //Aqui se desarrolla el bucle del menú
 
     switch (seleccion)
     {
@@ -96,12 +37,9 @@ void main()
         mododejuego3;
         break;
 
-    default:
+    default: return 0;
         break;
-
-
     }
-
 
 }
 
