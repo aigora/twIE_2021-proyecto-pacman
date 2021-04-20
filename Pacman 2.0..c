10ef92d4ed7615d1e4f81ed9c4de1455e795e496
Alvaro Lopez-Hermida <alvaro.lopez-hermida@alumnos.upm.es>
2021-04-20 16:31:52 +0200

    Cambio1 20/4/2021
    He empezado a definir la estructura en la que se guardara la puntuación del jugador y el fichero en el que se presentará.

--- Pacman 2.0..c
+++ Pacman 2.0..c
@@ -1,27 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//Aqui definimos las funciones de los tres modos de juego
+//Aqui definimos las funciones de los dos modos de juego
 
 int mododejuego1(int a, int b);
 int mododejuego2(int c, int d);
-int mododejuego3(int e, int f);
+
+//funcion que genera el mapa en pantalla
+
+void imprimir_mapa();
+
+//estructura en la que se generaran las puntuaciones
+
+typedef struct{
+    char nombre[10];
+    int puntos;
+}puntuacion;
 
 
 int main()
 {
     int seleccion;
 
+    puntuacion jugador1;
+
+    FILE* fichero_puntuacion;
+
+
     //Aqui el menu nos da a elegir entre los 3 modos de juego
 
     printf("***************************************************************\n\n");
-    printf("                   Eliga el modo de juego                    \n\n");
-    printf("  -Para modo de juego 1 pulse 1 e intro\n");
-    printf("  -Para modo de juego 2 pulse 2 e intro\n");
-    printf("  -Para modo de juego 3 pulse 3 e intro\n");
+    printf("                   Eliga el modo de juego                    \n\n\n");
+    printf("   Escriba el numero del modo de juego y seguido su nombre: 1 manuel\n\n");
+    printf("  -Para modo de juego 1 (Clasico) escriba 1\n\n");
+    printf("  -Para modo de juego 2 (Caza a los fantasmas) escriba 2\n\n");
     printf("  -Para cerrar programa pulse cualquier otra tecla e intro\n\n");
     printf("***************************************************************\n");
 
-    scanf("%i",&seleccion);
+    scanf("%i %s",&seleccion, jugador1.nombre);
 
     //Aqui se desarrolla el bucle del menú
 
@@ -33,13 +49,33 @@ int main()
     case 2:
         mododejuego2;
         break;
-    case 3:
-        mododejuego3;
-        break;
-
     default: return 0;
         break;
     }
+
+    fichero_puntuacion = fopen("Puntuacion Jugador1.txt","wt");
+    fputs(jugador1.puntos, fichero_puntuacion);
+    fputc(jugador1.nombre, fichero_puntuacion);
+    fclose(fichero_puntuacion);
+
+    return 0;
+}
+
+//desarrollo de las funciones principales
+
+
+int mododejuego1(a, b)
+{
+
+}
+
+int mododejuego2(c, d)
+{
+
+}
+
+void imprimir_mapa()
+{
 
 }
 
