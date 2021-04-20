7a8fbd38fd91359ed15ed50b3a40dd5eecadc1e3
Alvaro Lopez-Hermida <alvaro.lopez-hermida@alumnos.upm.es>
2021-04-20 18:18:36 +0200

    Cambio 2 20/4/2021
    Hemos creado el mapa y la funcion de imprimirlo en pantalla

--- Pacman 2.0..c
+++ Pacman 2.0..c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 //Aqui definimos las funciones de los dos modos de juego
 
 int mododejuego1(int a, int b);
 int mododejuego2(int c, int d);
 
-//funcion que genera el mapa en pantalla
+//definimos la funcion que genera el mapa en pantalla
 
 void imprimir_mapa();
+
+//aqui definimos la matriz mapa
+
+char mapa[20][31]={
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    "X           XXXXX           X"
+    "X XXX XXXXX XXXXX XXXXX XXX X"
+    "X XXX XXXXX XXXXX XXXXX XXX X"
+    "X                           X"
+    "X XXX XX XXXXXXXXXXX XX XXX X"
+    "X     XX     XXX     XX     X"
+    "X XXX XXXXXX XXX XXXXXX XXX X"
+    "X XXX XX             XX XXX X"
+    "      XX XXXXXXXXXXX XX      "
+    "X XXX XX XXXXXXXXXXX XX XXX X"
+    "X XXX XX             XX XXX X"
+    "X XXX XXXXXX XXX XXXXXX XXX X"
+    "X     XX     XXX     XX     X"
+    "X XXX XX XXXXXXXXXXX XX XXX X"
+    "X                           X"
+    "X XXX XXXXX XXXXX XXXXX XXX X"
+    "X XXX XXXXX XXXXX XXXXX XXX X"
+    "X           XXXXX           X"
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+};
 
 //estructura en la que se generaran las puntuaciones
 
@@ -17,6 +43,7 @@ typedef struct{
     int puntos;
 }puntuacion;
 
+//funcion main
 
 int main()
 {
@@ -24,7 +51,7 @@ int main()
 
     puntuacion jugador1;
 
-    FILE* fichero_puntuacion;
+    FILE *fichero_puntuacion;
 
 
     //Aqui el menu nos da a elegir entre los 3 modos de juego
@@ -36,8 +63,7 @@ int main()
     printf("  -Para modo de juego 2 (Caza a los fantasmas) escriba 2\n\n");
     printf("  -Para cerrar programa pulse cualquier otra tecla e intro\n\n");
     printf("***************************************************************\n");
-
-    scanf("%i %s",&seleccion, jugador1.nombre);
+    scanf("%i %s",&seleccion, &jugador1.nombre);
 
     //Aqui se desarrolla el bucle del menú
 
@@ -53,9 +79,10 @@ int main()
         break;
     }
 
-    fichero_puntuacion = fopen("Puntuacion Jugador1.txt","wt");
-    fputs(jugador1.puntos, fichero_puntuacion);
-    fputc(jugador1.nombre, fichero_puntuacion);
+    fichero_puntuacion = fopen("Puntuacion Jugador1.txt","w");
+    fprintf(fichero_puntuacion,"Puntuaciones Jugador 1:\n");
+    fprintf(fichero_puntuacion,"Nombre: %s\n", jugador1.nombre);
+    fprintf(fichero_puntuacion,"Puntuacion: %i\n", jugador1.puntos);
     fclose(fichero_puntuacion);
 
     return 0;
@@ -76,6 +103,12 @@ int mododejuego2(c, d)
 
 void imprimir_mapa()
 {
+    int fila, columna;
+    int maximofilas = 20;
+    int maximocolumnas = 31;
 
+    for(fila=0 ; fila<maximofilas ; fila++){
+        for(columna=0 ; columna<maximocolumnas ; columna++);
+    }
 }
 
